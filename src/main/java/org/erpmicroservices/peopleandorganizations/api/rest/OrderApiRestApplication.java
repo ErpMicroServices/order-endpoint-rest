f08954825f79dc915ad7dd519707869611127cd0
Jim Barrows <Jim.Barrows@gmail.com>
2020-06-18 17:49:37 -0500

    Initial commit.

---
+++ src/main/java/org/erpmicroservices/peopleandorganizations/api/rest/OrderApiRestApplication.java
@@ -0,0 +1,12 @@
+package org.erpmicroservices.peopleandorganizations.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApiRestApplication {
+
+ public static void main(String[] args) {
+	SpringApplication.run(OrderApiRestApplication.class, args);
+ }
+}
